3a1d71fd915a076bdc1d4d562f4f5f5bc0910b6d
Borislava Bagaliyska <bbagaliyska@vmware.com>
2025-01-04 23:31:19 +0200

    Extract labels and implement tokenizing processor

--- research_classifier/preprocessing/processors.py
+++ research_classifier/preprocessing/processors.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 from nltk.corpus import stopwords
 import nltk
+from transformers import AutoTokenizer
 
 
 class AbstractProcessor(ABC):
@@ -23,3 +24,17 @@ class StopWordRemover(AbstractProcessor):
                 and len(word.strip()) > 0
             ]
         )
+
+
+class DefaultTokenizer(AbstractProcessor):
+    def __init__(self):
+        self.tokenizer = AutoTokenizer.from_pretrained("bert-base-cased", is_fast=True)
+
+    def process(self, abstract: str) -> str:
+        # do not pad at this stage to max length,
+        # instead use dynamic padding to max sequence length in batch
+        # when batching with data collator
+        #
+        # truncation should be good enough as abstracts are rarely longer than 512 tokens
+        # and even when they are, they don't exceed by much
+        return self.tokenizer(abstract, truncation=True)
