4dfb2ac95527f81d78995ee2978f8d0530be467c
Borislava Bagaliyska <bbagaliyska@vmware.com>
2025-01-08 12:49:19 +0200

    Include entrypoint for evaluate for Google Colab

---
+++ research_classifier/training/evaluate.py
@@ -0,0 +1,16 @@
+import sys
+from research_classifier.training.trainer import evaluate
+
+# file to expose an entrypoint for the evaluation script
+if __name__ == "__main__":
+    if len(sys.argv) < 4:
+        print("Usage: python evaluate.py <model_path> <output_dir> <dataset_path>")
+        sys.exit(1)
+    model_path = sys.argv[1]
+    output_dir = sys.argv[2]
+    dataset_path = sys.argv[3]
+    evaluate(
+        model_path=model_path,
+        output_dir="./output",
+        dataset_path="./data/undersampled_dataset.json",
+    )
